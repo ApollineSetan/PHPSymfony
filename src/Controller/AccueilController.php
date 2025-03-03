fdaa7e68fccb747e24133d7eafd7344f5e1c245e
ApollineSetan <setan.apolline@gmail.com>
2025-03-03 16:59:05 +0100

    added style

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -23,36 +23,37 @@ class AccueilController {
     }
 
     #[Route(path: '/accueil/{nbr1}/{operateur}/{nbr2}', name:'app_accueil_calculatrice')]
-    public function calculatrice(mixed $nbr1, string $operateur, mixed $nbr2): Response {
+    public function calculatrice(mixed $nbr1, $operateur, mixed $nbr2): Response {
 
-        $resultat = "";
-
-        switch ($operateur) {
-            case "add":
-                $resultat = "<p>L’addition de " . $nbr1 . " et " . $nbr2 . " est égale à : " . ($nbr1 + $nbr2) . "</p>";
-                break;
-
-            case "sous":
-                $resultat = "<p>La soustraction de " . $nbr1 . " et " . $nbr2 . " est égale à : " . ($nbr1 - $nbr2) . "</p>";
-                break;
+        if (!is_numeric($nbr1) || !is_numeric($nbr2)){
+            $resultat = "Veuillez entrer des nombres numériques.";
+        } else {
+            switch ($operateur) {
+                case "add":
+                    $resultat = "<p>L’addition de " . $nbr1 . " et " . $nbr2 . " est égale à : " . ($nbr1 + $nbr2) . "</p>";
+                    break;
     
-            case "multi":
-                $resultat = "<p>La multiplication de " . $nbr1 . " par " . $nbr2 . " est égale à : " . ($nbr1 * $nbr2) . "</p>";
-                break;
-    
-            case "div":
-                if ($nbr1 == 0 || $nbr2 == 0) {
-                    $resultat = "<p>Erreur : Division par zéro impossible.</p>";
-                } else {
-                    $resultat = "<p>La division de " . $nbr1 . " par " . $nbr2 . " est égale à : " . ($nbr1 / $nbr2) . "</p>";
-                }
-                break;
-    
-            default:
-                $resultat = "<p>Erreur : opérateur incorrect.</p>";
-                break;
+                case "sous":
+                    $resultat = "<p>La soustraction de " . $nbr1 . " et " . $nbr2 . " est égale à : " . ($nbr1 - $nbr2) . "</p>";
+                    break;
+        
+                case "multi":
+                    $resultat = "<p>La multiplication de " . $nbr1 . " par " . $nbr2 . " est égale à : " . ($nbr1 * $nbr2) . "</p>";
+                    break;
+        
+                case "div":
+                    if ($nbr1 == 0 || $nbr2 == 0) {
+                        $resultat = "<p>Erreur : Division par zéro impossible.</p>";
+                    } else {
+                        $resultat = "<p>La division de " . $nbr1 . " par " . $nbr2 . " est égale à : " . ($nbr1 / $nbr2) . "</p>";
+                    }
+                    break;
+        
+                default:
+                    $resultat = "<p>Erreur : opérateur incorrect.</p>";
+                    break;
+            }
         }
-
         return new Response($resultat);
     }
 }
